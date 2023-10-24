c10186aff73848c86fd2855f2a618e595eb34040
kutumov.n <kutumov.n@neftm.ru>
2023-10-24 15:11:59 +0300

    report_cve_nullability

--- src/core/KutCode.Cve.Application/Database/Migrations/20231024121115_report_cve_nullability.Designer.cs
+++ src/core/KutCode.Cve.Application/Database/Migrations/20231024121115_report_cve_nullability.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KutCode.Cve.Application.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KutCode.Cve.Application.Database.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024121115_report_cve_nullability")]
+    partial class report_cve_nullability
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/core/KutCode.Cve.Application/Database/Migrations/20231024121115_report_cve_nullability.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KutCode.Cve.Application.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class report_cve_nullability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "software",
+                table: "report_request_cve",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "platform",
+                table: "report_request_cve",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "software",
+                table: "report_request_cve",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "platform",
+                table: "report_request_cve",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
