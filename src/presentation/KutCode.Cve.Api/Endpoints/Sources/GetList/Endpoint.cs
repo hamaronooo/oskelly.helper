96da770cc357873d00f7f0c4595c8d3bae352463
kutumov.n <kutumov.n@neftm.ru>
2023-11-08 18:07:28 +0300

    sources api started

---
+++ src/presentation/KutCode.Cve.Api/Endpoints/Sources/GetList/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using KutCode.Cve.Application.Interfaces.Cve;
+
+namespace KutCode.Cve.Api.Endpoints.Sources.GetList;
+
+public sealed class Endpoint : EndpointWithoutRequest<IEnumerable<Response>>
+{
+	public override void Configure()
+	{
+		Version(1);
+		AllowAnonymous();
+		Get("sources/list");
+	}
+
+	public override async Task HandleAsync(CancellationToken ct)
+	{
+		var type = typeof(ICveResolver);
+		var types = AppDomain.CurrentDomain.GetAssemblies()
+			.SelectMany(s => s.GetTypes())
+			.Where(p => type.IsAssignableFrom(p));
+		foreach (var t in types) {
+			var a = t.GetProperty("Code").GetConstantValue();
+		}
+		await SendOkAsync(new []{}, ct);
+	}
+}
---
+++ src/presentation/KutCode.Cve.Api/Endpoints/Sources/GetList/Response.cs
@@ -0,0 +1,8 @@
+﻿
+namespace KutCode.Cve.Api.Endpoints.Sources.GetList;
+
+public sealed class Response
+{
+	public string Code { get; set; }
+	public string Name { get; set; }
+}
